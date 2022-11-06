b4be052ff481f80ce568ff2c050f787b43b851fc
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-11-06 19:16:37 +0530

    logger enabled

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //jshint esversion:11
-
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
+//Global Middlewares
+app.use(morgan("dev"));
+app.use(express.json()); // Middleware to parse the request body
 
+//Routes
+
+
+//Start Server
 app.listen(3000, () => {
     console.log("App started on port 3000");
 });
