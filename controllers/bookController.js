e5dc91dec1472297061fa3ae924c54eecd728c7d
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-11-08 15:07:01 +0530

    controllers initialized

---
+++ controllers/bookController.js
@@ -0,0 +1,44 @@
+exports.getAllBooks = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getBook',
+    },
+  });
+};
+
+exports.getbook = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getBook',
+    },
+  });
+};
+
+exports.createBook = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getBook',
+    },
+  });
+};
+
+exports.updateBook = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getBook',
+    },
+  });
+};
+
+exports.deleteBook = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getBook',
+    },
+  });
+};
---
+++ controllers/entryController.js
@@ -0,0 +1,44 @@
+exports.getAllEntries = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getEntry',
+    },
+  });
+};
+
+exports.getEntry = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getEntry',
+    },
+  });
+};
+
+exports.createEntry = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getEntry',
+    },
+  });
+};
+
+exports.updateEntry = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getEntry',
+    },
+  });
+};
+
+exports.deleteEntry = async (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'from getEntry',
+    },
+  });
+};
