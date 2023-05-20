63761bd6f40d79b794bc4728125bed74f30f7656
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-20 17:33:13 +0530

    clean slate

--- models/entryModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const entrySchema = mongoose.Schema({
-  amount: Number,
-});
-
-const Entry = mongoose.model('Entry', entrySchema);
-
-module.exports = Entry;
--- readme.md
+++
@@ -1,3 +0,0 @@
-# DigiLedger
-
-This a budget management and transaction tracking system and handle our day to day transactions easily.
