8a9d0edc60d7a18bea74837c3e7cf0c60cfe3d0e
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-25 20:09:58 +0530

    query filtering

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,7 +2,50 @@ const Transaction = require('../models/transactionModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllTransactions = catchAsync(async (req, res, next) => {
-  const transctions = await Transaction.find();
+  //BUILD QUERY
+  // 1.Filtering
+  const queryObj = { ...req.query };
+  const excludedField = ['page', 'sort', 'limit', 'fields'];
+  excludedField.forEach((el) => delete queryObj[el]);
+
+  // 2.Query Operators
+  let queryString = JSON.stringify(queryObj);
+  queryString = queryString.replace(
+    /\b[gte|gt|lte|lt]\b/g,
+    (match) => `$${match}`
+  );
+
+  let query = Transaction.find(JSON.parse(queryString));
+  // 3.SORT
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  }
+
+  // 4.Limit Fields
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  }
+  query = query.select('-__v');
+
+  // 5. Pagination
+  const limit = req.query.limit * 1 || 10;
+  const page = req.query.page * 1 || 1;
+  const skip = (page - 1) * limit;
+
+  query = query.skip(skip).limit(limit);
+
+  if (req.query.page) {
+    const transactionCount = await Transaction.countDocuments();
+    if (skip >= transactionCount) throw new Error('PAge not found');
+  }
+
+  //EXECUTE QUERY
+  const transctions = await query;
+
+  //SEND QUERY
   res.status(200).json({
     status: 'success',
     data: {
