c8ea5498ba5c82a358a0f8f26de405597f3cb9ff
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-26 18:08:11 +0530

    error handling init

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 const transactionRouter = require('./routes/transactionRoutes');
 
 const app = express();
 
 //Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'dev') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/api/v1/transaction', transactionRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
