d8ee7ca11121770fa7a647c388851ecbf477c2d5
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-27 20:16:21 +0530

    update user data route

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const filtered = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      filtered[key] = obj[key];
+    }
+  });
+  return filtered;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //If password data, throw error
+  if (req.body.password || req.body.passwordConfirm || req.body.role) {
+    throw new AppError('This route is not for updating password or role', 400);
+  }
+
+  //Filer unwanted fields
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findById(req.user.id, filteredBody, {
+    runValidators: true,
+    new: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser,
+    },
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+
 router.patch(
   '/updateMyPassword',
   authController.protect,
   authController.updatePassword
 );
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 module.exports = router;
