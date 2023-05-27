0fe96462ca0f56c375814d33fd1463136919aeb5
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-27 11:30:47 +0530

    signup

--- config.js
+++ config.js
@@ -4,11 +4,15 @@ const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV;
 const database =
   env === 'dev' ? process.env.DATABASE_DEV : process.env.DATABASE_PROD;
+const jwtSecret = process.env.JWT_SECRET;
+const jwtExpire = process.env.JWT_EXPIRE;
 
 const config = {
   port,
   env,
   database,
+  jwtSecret,
+  jwtExpire,
 };
 
 module.exports = config;
