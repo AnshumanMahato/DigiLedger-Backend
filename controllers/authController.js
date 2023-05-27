9a756ef12dc1f0f19e7090dccf3f522866734887
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-27 12:03:52 +0530

    jwt set

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,13 @@
 const jwt = require('jsonwebtoken');
+const { jwtSecret, jwtExpire } = require('../config');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-const { jwtSecret, jwtExpire } = require('../config');
+const AppError = require('../utils/AppError');
+
+const signToken = (id) =>
+  jwt.sign({ id }, jwtSecret, {
+    expiresIn: jwtExpire,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -11,9 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, jwtSecret, {
-    expiresIn: jwtExpire,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -21,5 +25,32 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1. Check if email and password not null
+  if (!email || !password) {
+    throw new AppError('Please provide email and password', 400);
+  }
+
+  //2. Check if user exist
+  const user = await User.findOne({ email }).select('+password');
+
+  //3. Check if password match
+  const correct = await user?.checkPassword(password, user.password);
+
+  if (!user || !correct) {
+    throw new AppError('Incorrect email or password', 401);
+  }
+
+  //4. Sign token
+  const token = signToken(user._id);
+  //5. send response
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
