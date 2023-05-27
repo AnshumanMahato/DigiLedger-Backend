33e1dfd8ce25cd6d1f51472f1545c0cfb1753e60
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-28 05:06:10 +0530

    fix db-init script trying to query befor established connection

--- scripts/db-init.js
+++ scripts/db-init.js
@@ -5,21 +5,14 @@ const { database } = require('../config');
 const Transaction = require('../models/transactionModel');
 const User = require('../models/userModel');
 
-mongoose
-  .connect(database)
-  .then(() => {
-    console.log('DB Connected');
-  })
-  .catch(() => console.log('Failed to connect'));
-
-const users = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8')
-);
-const transactions = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../data/transactions.json'), 'utf-8')
-);
-
 async function importData() {
+  const users = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8')
+  );
+  const transactions = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../data/transactions.json'), 'utf-8')
+  );
+
   try {
     await Transaction.create(transactions);
     await User.create(users, { validateBeforeSave: false });
@@ -29,6 +22,7 @@ async function importData() {
   }
   process.exit();
 }
+
 async function deleteData() {
   try {
     await Transaction.deleteMany();
@@ -40,15 +34,21 @@ async function deleteData() {
   process.exit();
 }
 
-switch (process.argv[2]) {
-  case '--import':
-    importData();
-    break;
-  case '--delete':
-    deleteData();
-    break;
-  default:
-    console.log('Please provide --import or --delete');
-    process.exit();
-    break;
-}
+mongoose
+  .connect(database)
+  .then(() => {
+    console.log('DB Connected');
+
+    switch (process.argv[2]) {
+      case '--import':
+        importData();
+        break;
+      case '--delete':
+        deleteData();
+        break;
+      default:
+        console.log('Please provide --import or --delete');
+        process.exit();
+    }
+  })
+  .catch(() => console.log('Failed to connect'));
